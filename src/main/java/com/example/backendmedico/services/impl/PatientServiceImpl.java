5a479096e2db0f7d44a13569b2485324521d7347
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2025-02-11 19:51:20 -0500

    feat: add patient pagination

--- src/main/java/com/example/backendmedico/services/impl/PatientServiceImpl.java
+++ src/main/java/com/example/backendmedico/services/impl/PatientServiceImpl.java
@@ -3,49 +3,66 @@ package com.example.backendmedico.services.impl;
 import com.example.backendmedico.dao.PatientDAO;
 import com.example.backendmedico.models.PatientModel;
 import com.example.backendmedico.services.PatientService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
 public class PatientServiceImpl implements PatientService {
 	private final PatientDAO patientDAO;
 
-	@Override
-	public List<PatientModel> getAllPatients() {
-		return patientDAO.getAllPatients();
+	public PatientServiceImpl(PatientDAO patientDAO) {
+		this.patientDAO = patientDAO;
 	}
 
 	@Override
-	public PatientModel getPatientById(final String patientId) {
+	public List<PatientModel> getAllPatients(Pageable page) {
+		return patientDAO.getAllPatients(page);
+	}
+
+	@Override
+	public PatientModel getPatientById(String patientId) {
 		return patientDAO.getPatientById(patientId);
 	}
 
 	@Override
-	public PatientModel addPatient(final PatientModel patient) {
+	public PatientModel addPatient(PatientModel patient) {
+		if (patient.getPatientId() == null || patient.getPatientId().isBlank()) {
+			patient.setPatientId(UUID.randomUUID().toString());
+		}
 		return patientDAO.addPatient(patient);
 	}
 
 	@Override
-	public PatientModel updatePatient(final String patientId, final PatientModel patient) {
-		return patientDAO.updatePatient(patientId, patient);
+	public PatientModel updatePatient(String patientId, PatientModel patient) {
+		PatientModel foundPatient = getPatientById(patient.getPatientId());
+
+		foundPatient.setFirstName(patient.getFirstName());
+		foundPatient.setLastName(patient.getLastName());
+		foundPatient.setEmail(patient.getEmail());
+		foundPatient.setPhone(patient.getPhone());
+		foundPatient.setAddress(patient.getAddress());
+		foundPatient.setDni(foundPatient.getDni());
+		foundPatient.setBirthDate(patient.getBirthDate());
+
+		return patientDAO.updatePatient(foundPatient);
 	}
 
 	@Override
-	public Integer deletePatient(final String patientId) {
+	public Integer deletePatient(String patientId) {
 		return patientDAO.deletePatient(patientId);
 	}
 
 	@Override
-	public PatientModel getPatientByDni(final String dni) {
+	public PatientModel getPatientByDni(String dni) {
 		return patientDAO.getPatientByDni(dni);
 	}
 
 	@Override
-	public List<PatientModel> filterPatients(final Map<String, String> valuesToFilter) {
-		return patientDAO.filterPatients(valuesToFilter);
+	public List<PatientModel> filterPatients(Map<String, String> valuesToFilter, Pageable page) {
+		return patientDAO.filterPatients(valuesToFilter, page);
 	}
 }
